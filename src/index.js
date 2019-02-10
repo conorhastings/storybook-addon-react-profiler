d0909d770e5976a4320c5ab3ef17d599642dff07
Conor Hastings <hastings.conorm@gmail.com>
2019-02-10 09:19:40 -0800

    save previous renders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React, { unstable_Profiler as Profiler } from "react";
 import addons, { makeDecorator } from "@storybook/addons";
 
+function cacheProfiles() {
+  const profiles = {};
+  return function getProfiles(id, profile) {
+    if (profiles[id]) {
+      profiles[id].unshift(profile);
+    } else {
+      profiles[id] = [profile];
+    }
+    return profiles[id];
+  };
+}
+const getProfiles = cacheProfiles();
 export const withProfiler = makeDecorator({
   name: "withProfiler",
   parameterName: "profiler",
@@ -8,17 +20,19 @@ export const withProfiler = makeDecorator({
   wrapper: (getStory, context) => {
     const channel = addons.getChannel();
     function onRender(id, phase, actualTime, baseTime, startTime, commitTime) {
-      channel.emit("REACTPROFILERADDON/updateProfilerInfo", {
-        time: actualTime,
-        isInitialMount: phase !== 'update',
-        timeBetweenStartAndCommit: commitTime - startTime  
-      });
+      channel.emit(
+        "REACTPROFILERADDON/updateProfilerInfo",
+        getProfiles(id, {
+          id,
+          time: actualTime,
+          isInitialMount: phase !== "update",
+          timeBetweenStartAndCommit: commitTime - startTime
+        })
+      );
     }
     return (
-      <Profiler 
-        id={context.id} 
-        key="profiler" 
-        onRender={onRender}>{getStory(context)}
+      <Profiler id={context.id} key="profiler" onRender={onRender}>
+        {getStory(context)}
       </Profiler>
     );
   }
--- src/register.js
+++ src/register.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import addons from '@storybook/addons';
-import styled from '@emotion/styled';
+import React from "react";
+import addons from "@storybook/addons";
+import styled from "@emotion/styled";
+import { f } from "core-js/modules/_object-dp";
 
 const ProfilerPanel = styled.div({
   margin: 10,
-  width: '100%',
-  overflow: 'auto',
+  width: "100%",
+  overflow: "auto"
 });
 
 class ProfilerInfo extends React.Component {
   constructor() {
     super();
-    this.state = { data: {}};
+    this.state = { data: [] };
     this.onUpdateProfilerInfo = this.onUpdateProfilerInfo.bind(this);
   }
 
   componentDidMount() {
     const { channel, api } = this.props;
     // Listen to the notes and render it.
-    channel.on("REACTPROFILERADDON/updateProfilerInfo", this.onUpdateProfilerInfo);
+    channel.on(
+      "REACTPROFILERADDON/updateProfilerInfo",
+      this.onUpdateProfilerInfo
+    );
 
     // Clear the current notes on every story change.
     this.stopListeningOnStory = api.onStory(() => {
-      this.onUpdateProfilerInfo({});
+      this.onUpdateProfilerInfo([]);
     });
   }
 
@@ -31,24 +35,59 @@ class ProfilerInfo extends React.Component {
     if (this.stopListeningOnStory) {
       this.stopListeningOnStory();
     }
-    channel.removeListener('REACTPROFILERADDON/updateProfilerInfo', this.onUpdateProfilerInfo);
+    channel.removeListener(
+      "REACTPROFILERADDON/updateProfilerInfo",
+      this.onUpdateProfilerInfo
+    );
   }
-  
+
   onUpdateProfilerInfo(data) {
-    console.log(data);
+    console.log(this.node.scrollHeight, this.node.parentNode);
+    this.node.parentNode.scrollTop = this.node.scrollHeight;
     this.setState({ data });
   }
 
   render() {
-    const { data } = this.state;
-    const { active } = this.props;
-    console.log(data)
+    const { data: profiles } = this.state;
     return (
       <ProfilerPanel key="profilerPanel">
-        <div>
-          <div><strong>Is Update Or Initial Mount</strong>: {data.isIniitialMount ? 'Initial Mount' : 'Update'}</div>
-          <div><strong>Time To Render</strong>: {`${data.time} ms`}</div>
-          <div><strong>Difference Between Start Time And Commit Time</strong>(can be indicator of lag): {`${data.timeBetweenStartAndCommit} ms`}</div>
+        <div
+          style={{ width: 800, margin: "0 auto" }}
+          ref={node => (this.node = node)}
+        >
+          {profiles.map((data, index) => (
+            <div
+              key={`${data.id}-${index}`}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: 20,
+                marginBottom: 10,
+                borderRadius: 10,
+                width: "80%",
+                color: index === 0 ? "white" : "rgb(15,15,15)",
+                backgroundColor: index === 0 ? "#B481BB" : "rgb(241, 241, 241)"
+              }}
+            >
+              <div>
+                {index === 0 ? (
+                  <div style={{ position: "relative", top: -10, left: 0 }}>
+                    <strong>Latest</strong>
+                  </div>
+                ) : null}
+                <strong>Is Update Or Initial Mount</strong>:{" "}
+                {data.isIniitialMount ? "Initial Mount" : "Update"}
+              </div>
+              <div>
+                <strong>Time To Render</strong>: {`${data.time} ms`}
+              </div>
+              <div>
+                <strong>Difference Between Start Time And Commit Time</strong>
+                (can be indicator of lag):{" "}
+                {`${data.timeBetweenStartAndCommit} ms`}
+              </div>
+            </div>
+          ))}
         </div>
       </ProfilerPanel>
     );
@@ -56,11 +95,13 @@ class ProfilerInfo extends React.Component {
 }
 export default function register() {
   // Register the addon with a unique name.
-  addons.register('REACTPROFILERADDON', api => {
+  addons.register("REACTPROFILERADDON", api => {
     // Also need to set a unique name to the panel.
-    addons.addPanel('REACTPROFILERADDON/panel', {
-      title: 'Profiler',
-      render: ({ active }) => <ProfilerInfo channel={addons.getChannel()} api={api} active={active} />,
+    addons.addPanel("REACTPROFILERADDON/panel", {
+      title: "Profiler",
+      render: ({ active }) => (
+        <ProfilerInfo channel={addons.getChannel()} api={api} active={active} />
+      )
     });
   });
 }
